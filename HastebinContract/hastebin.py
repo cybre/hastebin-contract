2320ca48643ccd32260c4ea23cc6ea934e5f3294
Stefan Ric <stfric369@gmail.com>
2016-02-14 23:36:50 +0100

    Style and some more things

--- HastebinContract/hastebin.py
+++ HastebinContract/hastebin.py
@@ -1,83 +1,80 @@
 #!/usr/bin/python
-import sys
 from gi.repository import Gtk, Gdk, Notify
+import sys
 import urllib2
-
-appName = "Hastebin"
-
-msgOneFile = "Your file has been pasted."
-msgMultipleFiles = "Your files have been pasted."
-msgClipboard = "The link has been copied to your clipboard!"
-msgClipboardMultiple = "The links have been copied to your clipboard!"
-msgFailed = "Operation failed, try again later."
-
-# Notification icons
-iconInfo = "help-info"
-iconError = "error"
 
 
 class HastebinUploader:
-    def __init__(self, args):
+    def __init__ (self, args):
 
         files = []
 
         # Initialize the notification daemon
-        Notify.init(appName)
+        Notify.init ("Hastebin")
 
         # Parse arguments
-        if len(args) == 1:
+        if len (args) == 1:
             return
         else:
             for file in args:
                 if file == args[0] or file == "":
                     continue
-                files.append(file)
+                files.append (file)
 
-        self.paste(files)
+        self.paste_them_bad_boys (files)
 
-    def notify(self, messageOne, messageTwo, icon, timeout=5000):
+    def notify (self, message_one, message_two, icon):
         try:
-            self.notification = Notify.Notification.new(messageOne, messageTwo, icon)
-            self.notification.set_timeout(timeout)
-            self.notification.show()
+            notification = Notify.Notification.new (message_one, message_two, icon)
+            notification.set_urgency (1)
+            notification.show ()
+            del notification
         except:
             pass
 
-    def paste(self, files):
+    def paste_them_bad_boys (self, files):
         urls = []
-        URL = "http://hastebin.com"
+        URL  = "http://hastebin.com"
 
         for file in files:
-            filecontents = open(file, 'r')
+            filecontents = open (file, 'r')
             try:
-                data = "".join(filecontents.readlines()).strip()
+                data = "".join (filecontents.readlines ()).strip ()
             finally:
-                filecontents.close()
+                filecontents.close ()
 
             # Upload
-            request = urllib2.Request(URL + "/documents", data)
-            response = urllib2.urlopen(request)
-            rdata = response.read()
-            reval = eval(rdata)
+            request   = urllib2.Request (URL + "/documents", data)
+            response  = urllib2.urlopen (request)
+            read_data = response.read ()
+            read_eval = eval (read_data)
 
-            fileurl = "%s/%s" % (URL, reval['key'])
-            urls.append(fileurl)
+            fileurl = "{}/{}".format (URL, read_eval['key'])
+            urls.append (fileurl)
 
         # Copy to clipboard
-        urllist = ", ".join(urls)
-        print urllist
-        self.setClipboard(urllist)
-        if len(urls) > 1:
-            self.notify(msgMultipleFiles, msgClipboardMultiple, iconInfo, 3000)
+        url_list = ", ".join (urls)
+        print url_list
+        self.set_clipboard (url_list)
+        if len (urls) > 1:
+            self.notify (
+                "Your files have been pasted.", 
+                "The links have been copied to your clipboard!", 
+                "help-info"
+            )
         else:
-            self.notify(msgOneFile, msgClipboard, iconInfo, 3000)
+            self.notify (
+                "Your file has been pasted.", 
+                "The link has been copied to your clipboard!", 
+                "help-info"
+            )
 
-    def setClipboard(self, urllist):
-        display = Gdk.Display.get_default()
-        selection = Gdk.Atom.intern("CLIPBOARD", False)
-        clipboard = Gtk.Clipboard.get_for_display(display, selection)
-        clipboard.set_text(urllist, -1)
-        clipboard.store()
+    def set_clipboard (self, url_list):
+        display   = Gdk.Display.get_default()
+        selection = Gdk.Atom.intern ("CLIPBOARD", False)
+        clipboard = Gtk.Clipboard.get_for_display (display, selection)
+        clipboard.set_text (url_list, -1)
+        clipboard.store ()
 
 if __name__ == '__main__':
-    uploader = HastebinUploader(sys.argv)
+    uploader = HastebinUploader (sys.argv)
